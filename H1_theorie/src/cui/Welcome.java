68ce7243509f6256c32839f6548778343523f6c2
azime <azime@172.18.155.97>
2023-09-26 16:38:51 +0200

    26/9/2023

---
+++ H1_theorie/src/cui/Welcome.java
@@ -0,0 +1,23 @@
+package cui;
+
+public class Welcome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.print(2.5);
+		System.out.println("Welcome to");
+		System.out.println("Welcome to\njava programming");
+		// This is comment
+		//Snelle print = sysout, crtrl+spatie
+		System.out.printf("%s %d %.2f dit is een tekst%n", "Welkom", 123, 2.5 );
+		
+		System.out.printf("%S", "dit zal in hoofdletters staan\n");
+		
+		System.out.printf("%5d", 123);
+		System.out.printf("%5d%n", 123123);
+		System.out.printf("%5.2f", 2.5);
+		
+	}
+
+}
