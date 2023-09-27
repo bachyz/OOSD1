a192683727f7774653cc4e007ccfd8526129e81c
azime <azime@172.18.172.113>
2023-09-27 09:24:55 +0200

    26/09/2023 les 1

---
+++ MijnEersteProject/src/cui/Oefening2.java
@@ -0,0 +1,15 @@
+package cui;
+
+public class Oefening2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("Open de C:\\Mijn Documenten\nVolg de pijl \"Omleiding\"\nHet boek voor \tJava\nis dat van\t\tDeintel\nEngelstaling");
+		
+		
+		
+	}
+
+}
---
+++ MijnEersteProject/src/cui/Oefening3.java
@@ -0,0 +1,22 @@
+package cui;
+
+public class Oefening3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		int getal1 = 10;
+		int getal2 = 20;
+		double getal3 = 363.3636;
+		System.out.printf("%30s\t", "getal1 + getal2");
+		System.out.printf("%4d\n", getal1 + getal2);
+		System.out.printf("%d\t%f%n%3S%n", getal1 - getal2, getal3, "einde");
+		System.out.printf("%1d\n", 100 - getal2 * 10);
+		System.out.printf("\t%.3f", getal3);
+		
+		
+		
+	}
+
+}
