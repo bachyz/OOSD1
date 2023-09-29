1bfbd7b0efc10c65e3929882d8e821e66fe8bc9d
bachyz <mehdi.azibou@student.hogent.be>
2023-09-29 16:37:56 +0200

    ;jvkjgv

--- MijnEersteProject/src/cui/Oefening16.java
+++ MijnEersteProject/src/cui/Oefening16.java
@@ -1,10 +1,22 @@
 package cui;
+import java.util.*;
 
 public class Oefening16 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		int getal;
+		Scanner input = new Scanner(System.in);
+		System.out.println("Geef een geheel getal in : ");
+		getal = input.nextInt();
+		input.close();
+		System.out.printf("octale notatie : %o%n",getal);
+		System.out.printf("Hexadecimale notatie (klein): %x%n",getal);
+		System.out.printf("Hexadecimale notatie (groot): %X%n",getal);
 
+		
+		
 	}
 
 }
