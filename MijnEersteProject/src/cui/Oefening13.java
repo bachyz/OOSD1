6ab6176bd746d75dd1f1eb8846ab114195573a2d
bachyz <mehdi.azibou@student.hogent.be>
2023-09-29 16:03:07 +0200

    zefrdfgvb

--- MijnEersteProject/src/cui/Oefening13.java
+++ MijnEersteProject/src/cui/Oefening13.java
@@ -5,11 +5,16 @@ public class Oefening13 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		int datuming;
+		int datuming, dag, maand;
 		Scanner input = new Scanner(System.in);
 		System.out.println("Geef een datum in <ddmmjjjj>");
 		datuming = input.nextInt();
+		dag = datuming/1000000;
+		maand = (datuming%1000000)/10000;
+		
+		
 		input.close();
+		System.out.printf("Het duurt nog %d dagen voor het terug nieuwjaar is", ((30-dag)+((12-maand)*30)));
 	}
 
 }
