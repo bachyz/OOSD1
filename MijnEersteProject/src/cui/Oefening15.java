e5605508c09fccc60eb46b06e81f865be4a4833a
bachyz <mehdi.azibou@student.hogent.be>
2023-09-29 16:29:52 +0200

    czfregtrt

---
+++ MijnEersteProject/src/cui/Oefening15.java
@@ -0,0 +1,31 @@
+package cui;
+import java.util.*;
+public class Oefening15 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int getal1, getal2, getal3;
+		Scanner input = new Scanner(System.in);
+		System.out.println("Geef eerste getal1 : ");
+		getal1 = input.nextInt();
+		System.out.println("Geef tweede getal2 : ");
+		getal2 = input.nextInt();
+		System.out.println("Geef derde getal3 : ");
+		getal3 = input.nextInt();
+		input.close();
+		System.out.printf("De ingegeven getallen zijn %d, %d en %d",getal1, getal2, getal3 );
+		System.out.printf("\nDe som is : %d", getal1+getal2+getal3);
+		System.out.printf("\nHet gemiddelde is : %d", (getal1+getal2+getal3)/3);
+		System.out.printf("\nDe rest is : %d", (getal1+getal2+getal3)%3);
+
+
+
+
+
+		
+		
+		
+	}
+
+}
---
+++ MijnEersteProject/src/cui/Oefening16.java
@@ -0,0 +1,10 @@
+package cui;
+
+public class Oefening16 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
