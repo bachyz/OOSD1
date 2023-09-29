8d8acd496d8fe481d83d33de7b5005681fd9c6f1
bachyz <mehdi.azibou@student.hogent.be>
2023-09-29 15:51:17 +0200

    files added

---
+++ H1_theorie/src/cui/Telraam.java
@@ -0,0 +1,12 @@
+package cui;
+
+public class Telraam {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Test met concat: " + 1+5);
+		System.out.println("Test met optelling: " + (1+5));
+	}
+
+}
