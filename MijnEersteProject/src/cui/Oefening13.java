96e7d001b0e3a6d3a2f16e81c5f1541eed15fdf1
bachyz <mehdi.azibou@student.hogent.be>
2023-09-29 15:28:12 +0200

    efbgff

---
+++ MijnEersteProject/src/cui/Oefening13.java
@@ -0,0 +1,14 @@
+package cui;
+import java.util.Scanner;
+public class Oefening13 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int datuming;
+		Scanner input = new Scanner(System.in);
+		System.out.println("Geef een datum in <ddmmjjjj>");
+		
+	}
+
+}
