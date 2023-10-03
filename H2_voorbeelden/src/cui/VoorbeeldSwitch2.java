0429b75fbdb30f03e804aff7223ff9d007009fe2
bachyz <mehdi.azibou@student.hogent.be>
2023-10-03 16:09:35 +0200

    ;jv

--- H2_voorbeelden/src/cui/VoorbeeldSwitch2.java
+++ H2_voorbeelden/src/cui/VoorbeeldSwitch2.java
@@ -5,6 +5,12 @@ public class VoorbeeldSwitch2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		String stad = "Aalst";
+		switch (stad.charAt(0)){
+		case 'A' -> System.out.println("Aalst");
+		case 'B' -> System.out.println("Brugge");
+		case 'C' -> System.out.println("Gent");
+		}
 	}
 
 }
