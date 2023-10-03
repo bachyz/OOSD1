e5a9c1f8b956094b161cdb2b8cb7e859d40d88d1
bachyz <mehdi.azibou@student.hogent.be>
2023-10-03 14:32:01 +0200

    dedfr

---
+++ H2_voorbeelden/src/cui/SelectieStructuren.java
@@ -0,0 +1,22 @@
+package cui;
+import java.util.*;
+public class SelectieStructuren {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner input = new Scanner(System.in);
+		int number1;
+		System.out.printf("Geef een geheel getal in : ");
+		input.close();
+		number1 = input.nextInt();
+		
+		if (number1 > 5) {
+			System.out.println("Het getal is groter dan 5.");
+			
+		}
+		
+		
+	}
+
+}
