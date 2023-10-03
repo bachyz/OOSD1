a145ac8baf167f885d30cb3abd430752fc995f79
bachyz <mehdi.azibou@student.hogent.be>
2023-10-03 14:00:52 +0200

    H2

---
+++ H02_theorie/src/cui/DoWhileControle1.java
@@ -0,0 +1,27 @@
+package cui;
+
+import java.util.Scanner;
+
+public class DoWhileControle1
+{
+	public static void main(String[] args)
+	{
+		new DoWhileControle1().controleerInvoer();
+	}
+	
+	private void controleerInvoer()
+	{
+		Scanner input = new Scanner(System.in);
+		input.close();
+
+		int getal;
+
+		do // INVOERCONTROLE <1>
+		{ // <2>
+			System.out.print("Geef een strikt positief geheel getal in: ");
+			getal = input.nextInt();
+		} while (getal <= 0); // einde do/while structuur <3>
+		
+		System.out.printf("Het ingevoerde getal = %d", getal);
+	}
+}
