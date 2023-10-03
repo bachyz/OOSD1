db2cfe957e18281adbf3d3eeb9d601c359716ea4
bachyz <mehdi.azibou@student.hogent.be>
2023-10-03 13:44:34 +0200

    de

---
+++ MijnEersteProject/src/cui/som.java
@@ -0,0 +1,15 @@
+package cui;
+
+public class som {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		int x = 3, y = 5, som;
+		som = x * x + y * y;
+		System.out.println("Som = " + som);
+	}
+
+}
