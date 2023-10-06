7ff4be61a6321d17dd9372748f9486feaf265afd
bachyz <mehdi.azibou@student.hogent.be>
2023-10-06 16:34:40 +0200

    edzc

---
+++ H2_oefening/src/cui/oefening10b.java
@@ -0,0 +1,47 @@
+package cui;
+
+import java.util.Scanner;
+
+public class oefening10b {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+	}
+	
+	private void startOef10b() {
+		int getal = geefGetal();
+		int kleinste = getal;
+		int grootste = getal;
+		
+		while(getal != 0) {
+			//verwerking
+			if(getal > grootste) {
+				grootste = getal;
+			}
+			if(getal < kleinste){
+				kleinste = getal;
+			}			
+			//nieuwe waarde
+			getal = geefGetal();
+			
+		}		
+		
+		if(grootste == 0) {
+			System.out.println("er werden geen geldige getallen ngevoerd");
+		}else {
+			System.out.printf("Het grootste getal is %d, en het kleinste is %d", grootste, kleinste);
+		}
+	}
+	private int geefGetal() {
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("Geef een getal in (0 om te stoppen)");
+		
+		return input.nextInt();
+	}
+	}
+
+}
