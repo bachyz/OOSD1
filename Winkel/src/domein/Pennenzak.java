19b7da9d07fac7e95441d6e447b9d8c08ecf5ede
bachyz <mehdi.azibou@student.hogent.be>
2023-10-17 14:58:53 +0200

    ,nbv

--- Winkel/src/domein/Pennenzak.java
+++ Winkel/src/domein/Pennenzak.java
@@ -2,12 +2,10 @@ package domein;
 
 public class Pennenzak {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 		private int inhoud;
 		private String kleur;
 		private int aantaalPotloden;
 	}
 
-}
+
